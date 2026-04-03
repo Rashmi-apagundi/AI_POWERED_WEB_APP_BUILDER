7cc2ecdf7af60e4773fe120e6e0ec89b2dbbbc6a
niat-web <psm@nxtwave.in>
2026-04-03 23:38:04 +0530

    frontend ready for deployment

--- .history/server/src/app_20260322142633.js
+++ .history/server/src/app_20260322142633.js
@@ -5,7 +5,7 @@ import { errorHandler, notFoundHandler } from './middleware/error.middleware.js'
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173',credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
 app.use(express.json({ limit: '10mb' }));
 
 app.use('/api', routes);
